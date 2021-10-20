d89f76c4e7d19a2f842f4a6ded5ae5106328cc6b
Kostas <skenderidis@gmail.com>
2021-10-20 07:03:49 +0000

    minor changes

--- Demo-1/gslb/main.tf
+++ Demo-1/gslb/main.tf
@@ -9,12 +9,11 @@ module "gslb-us" {
 
 resource "null_resource" "GSLB-info" {
   provisioner "local-exec" {
-    command = "echo '{\"gslb-us-dns-ip\":\"${module.gslb-us.F5_Ext_Public_IP}\", \"gslb-us-mgmt-ip\":\"${module.gslb-us.F5_Mgmt_Public_IP}\", \"f5_user\":\"${module.gslb-us.f5_username}\", \"f5_pass\":\"${module.gslb-us.f5_password}\"}' > ../gslb_info.json"
+    command = "echo '{\"gslb-us-dns-ip\":\"${module.gslb-us.F5_Ext_Public_IP}\", \"gslb-us-mgmt-ip\":\"${module.gslb-us.F5_Mgmt_Public_IP}\", \"f5_user\":\"${module.gslb-us.f5_username}\", \"f5_pass\":\"${module.gslb-us.f5_password}\"}' > /tmp/gslb_info.json"
   }
   provisioner "local-exec" {
     when    = destroy
-    command = "rm ../../gslb_info.json"
+    command = "rm /tmp/gslb_info.json"
     on_failure = continue
   }
 }
-

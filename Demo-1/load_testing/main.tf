b472695f89e3f6cffc8f19b0d65f51ee4fc4581f
Kostas <skenderidis@gmail.com>
2021-10-31 15:01:29 +0000

    Demo-1

---
+++ Demo-1/load_testing/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_group" "master" {
+  name                = "worker"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "public"
+  os_type             = "Linux"
+
+  container {
+    name   = "locust-worker"
+    image  = "skenderidis/test3"
+    cpu    = "2"
+    memory = "4"
+    commands = ["--worker --master-host=monitor.f5demo.cloud"]
+
+    ports {
+      port     = 8089, 5557
+      protocol = "TCP"
+    }
+  }
+
+}

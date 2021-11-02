b4d83e27328f43f5447ec00bf847ca3595fa2efa
Kostas <skenderidis@gmail.com>
2021-11-02 05:09:48 +0000

    Demo-1

--- Demo-1/load_testing/variables.tf
+++ Demo-1/load_testing/variables.tf
@@ -14,9 +14,9 @@ variable location				{default = "eastus"}
 
 
 variable count_eastus				{default = 1}
-variable count_westus				{default = 1}
+variable count_westus				{default = 0}
 variable count_northeu				{default = 0}
 variable count_westeu				{default = 1}
-variable count_eastasia				{default = 1}
+variable count_eastasia				{default = 0}
 
 

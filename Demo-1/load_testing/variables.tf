d1814a61b8c243cfc3a08b63a5f26196db2af270
Kostas Skenderidis <skenderidis@gmail.com>
2021-11-04 17:06:48 +0400

    Update variables.tf

--- Demo-1/load_testing/variables.tf
+++ Demo-1/load_testing/variables.tf
@@ -13,8 +13,8 @@ variable tenant_id				{}
 variable location				{default = "eastus"}
 
 
-variable count_eastus				{default = 0}
-variable count_westus				{default = 0}
+variable count_eastus				{default = 1}
+variable count_westus				{default = 1}
 variable count_northeu				{default = 1}
 variable count_uksouth				{default = 1}
 variable count_eastasia				{default = 1}
